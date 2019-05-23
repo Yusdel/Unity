71307b3ec3566b372e68062273fce26fc3b1275f
Yusdel <yusdel@live.it>
2019-05-23 18:14:36 +0200

    firstProject

---
+++ New Unity Project (1)/Assets/scripts/Background.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+	public float speed;
+	private Renderer renderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+    	renderer = gameObject.GetComponent<Renderer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float offset = speed * Time.deltaTime;
+    	Vector2 oldTexture = renderer.material.GetTextureOffset("_MainTex");
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, oldTexture.y + offset));
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ New Unity Project (1)/Assets/scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	float moveVertical = Input.GetAxis("Vertical");
+    	float offsetX = speed * moveHorizontal * Time.deltaTime;
+    	float offsetZ = speed * moveVertical * Time.deltaTime;
+
+    	transform.position = new Vector3(
+    		transform.position.x + offsetX,
+    		transform.position.y,
+    		transform.position.z + offsetZ);
+        
+    }
+}
